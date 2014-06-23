b0640e2ad71c070f96cc6b44b68ac0e932f6ec12
Alexander Sessler <alex-sessler@web.de>
2014-06-23 20:32:04 +0200

    test

--- src/test/java/backgammon04/backgammon04_persistence_mongodb/model/dao/impl/UserDaoImplTest.java
+++ src/test/java/backgammon04/backgammon04_persistence_mongodb/model/dao/impl/UserDaoImplTest.java
@@ -32,16 +32,40 @@ public class UserDaoImplTest {
 	@Test
 	public void test() {
 		User user = new UserImpl();
-		user.setEmail("alex@alex.de");
-		user.setPassword("alex");
-		user.setUsername("alex");
+
+		user.setEmail("alex2@alex.de");
+		user.setPassword("alex2");
+		user.setUsername("alex2");
 		userDao.save(user);
-		System.out.println(userDao);
-		User dbUser = userDao.getUser("alex");
+		User dbUser = userDao.getUser("alex2");
 		Assert.assertNotNull(dbUser);
+
+		User user2 = new UserImpl();
+		user2.setEmail("alex@alex2.de");
+		user2.setPassword("alex2");
+		user2.setUsername("alex2");
+		userDao.save(user2);
+		User dbUser2 = userDao.getUser("alex2");
 
 		userDao.delete(dbUser);
 		Assert.assertNull(userDao.getUser("alex"));
+	}
+
+	@Test
+	public void createMultiple() {
+		for (int i = 0; i < 10; i++) {
+			User user = new UserImpl();
+			user.setId(Long.valueOf(i));
+			user.setEmail("alex@alex" + i + ".de");
+			user.setPassword("alex" + i);
+			user.setUsername("alex" + i);
+			userDao.save(user);
+		}
+
+		for (int i = 0; i < 10; i++) {
+			User user = userDao.getUser("alex" + i);
+			userDao.delete(user);
+		}
 	}
 
 }
