d2e0fad2634b8e81686a1aa123e8883c8f40b979
Alexander Sessler <alex-sessler@web.de>
2014-06-25 01:16:14 +0200

    fix

--- src/main/java/backgammon04/backgammon04_persistence_mongodb/model/impl/UserImpl.java
+++ src/main/java/backgammon04/backgammon04_persistence_mongodb/model/impl/UserImpl.java
@@ -1,9 +1,5 @@
 package backgammon04.backgammon04_persistence_mongodb.model.impl;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,8 +9,6 @@ import backgammon04.backgammon04_persistence_interface.model.User;
 public class UserImpl implements User {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id", nullable = false, unique = true)
 	private long id;
 
 	private String username;
